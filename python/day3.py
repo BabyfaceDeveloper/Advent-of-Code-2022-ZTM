f9ad2ab52aa78e16e141fa00ed463daf9800e965
Nguyen <babyfacedeveloper@gmail.com>
2022-12-03 17:03:51 -0500

    day 3 part 2

--- python/day3.py
+++ python/day3.py
@@ -1,18 +1,29 @@
+from functools import reduce
+
+
 def get_priority(item: str) -> int:
     # 96 for lower case ascii, 38 = 26 - 64 (upper case)
     return ord(item[0]) - (96 if item[0].islower() else 38)
 
 
 with open("../inputs/day3.txt") as f:
-    sacks = f.readlines()
+    sacks = list(map(lambda sack: sack.strip(), f.readlines()))
 
+# Part 1
 sum_priorities = 0
 
 for sack in sacks:
     first_compartments = set(sack[: len(sack) // 2])
     second_compartments = set(sack[len(sack) // 2 :])
+    sum_priorities += get_priority((first_compartments & second_compartments).pop())
+
+print(sum_priorities)
+
+# Part 2
+sum_priorities = 0
+for i in range(0, len(sacks), 3):
     sum_priorities += get_priority(
-        first_compartments.intersection(second_compartments).pop()
+        (set(sacks[i]) & set(sacks[i + 1]) & set(sacks[i + 2])).pop()
     )
 
 print(sum_priorities)
